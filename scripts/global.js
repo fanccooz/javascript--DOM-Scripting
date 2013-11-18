16a7f921c6aafb871bef8d142f428de8f3e66355
John Doe <fangfang.ouyang@upai.com>
2013-11-18 15:57:42 +0800

    change js

--- scripts/global.js
+++ scripts/global.js
@@ -3,9 +3,9 @@
 function addLoadEvent(func){
 	var oldonload = window.onload;
 	if(typeof window.onload != "function"){
-		window.onload =func;
+		window.onload = func;
 	}else{
-		window.onload =function(){
+		window.onload = function(){
 			oldonload();
 			func();
 		}
@@ -24,8 +24,8 @@ function addClass(element,value){
 		element.className = value;
 	}else{
 		newClassName = element.className;
-		newClassName+="";
-		newClassName+=value;
+		newClassName += "";
+		newClassName += value;
 		element.className = newClassName;
 	}
 };
@@ -36,18 +36,18 @@ function addClass(element,value){
 function highlightPage(){
 	if(!document.getElementsByTagName) return false;
 	if(!document.getElementById) return false;
-	var headers=document.getElementsByTagName('header');
+	var headers = document.getElementsByTagName('header');
 	if(headers.length == 0) return false;
-	var navs=document.getElementsByTagName('nav');
+	var navs = document.getElementsByTagName('nav');
 	if(navs.length == 0) return false;
-	var links=document.getElementsByTagName('a');
+	var links = document.getElementsByTagName('a');
 	for (var i = 0; i < links.length; i++) {
 		var linkurl;
 		for (var i = 0; i < links.length; i++) {
-			linkurl=links[i].href;
+			linkurl = links[i].href;
 			if(window.location.href.indexOf(linkurl) !=-1){
-				links[i].className='here';
-				var linktext=links[i].lastChild.nodeValue.toLowerCase();
+				links[i].className = 'here';
+				var linktext = links[i].lastChild.nodeValue.toLowerCase();
 				document.body.setAttribute('id',linktext);
 			}
 			// console.log(linkurl);
@@ -63,7 +63,7 @@ addLoadEvent(highlightPage);
 function moveElement(elementID,final_x,final_y,interval){
 	if(!document.getElementsByTagName) return false;
 	if(!document.getElementById(elementID)) return false;
-	var elem=document.getElementById(elementID);
+	var elem = document.getElementById(elementID);
 	if(elem.movement){
 		clearTimeout(elem.movement);
 	}
@@ -73,7 +73,7 @@ function moveElement(elementID,final_x,final_y,interval){
 	if(!elem.style.left.top){
 		elem.style.top="0px";
 	}
-	var xpos=parseInt(elem.style.left);
+	var xpos = parseInt(elem.style.left);
 	var ypos = parseInt(elem.style.top);
 	if (xpos == final_x && ypos == final_y) {
 		return true;
@@ -95,7 +95,7 @@ function moveElement(elementID,final_x,final_y,interval){
 		ypos = ypos - dist;
 	}
 	elem.style.left = xpos +'px';
-	elem.style.top =ypos + 'px';
+	elem.style.top = ypos + 'px';
 	var repeat = "moveElement("+elementID+","+final_x+","+final_y+","+interval+")";
 	elem.movement = setTimeout(repeat,setInterval);
 };
@@ -104,21 +104,21 @@ function prepareSlideshow(){
 	if (!document.getElementsByTagName) return false;
 	if (!document.getElementById) return false;
 	if (!document.getElementById('intro')) return false;
-	var intro=document.getElementById('intro');
-	var slideshow=document.createElement('div');
+	var intro = document.getElementById('intro');
+	var slideshow = document.createElement('div');
 	slideshow.setAttribute('id','slideshow');
 	var preview = document.createElement('img');
 	preview.setAttribute('src','images/fire.png');
 	preview.setAttribute('alt','a glimpse of what awaits you')
 	preview.setAttribute('id','preview');
-	preview.style.width='750px';
+	preview.style.width = '750px';
 	slideshow.appendChild(preview);
 	insertAfter(slideshow,intro);
-	var links=intro.getElementsByTagName('a');
+	var links = intro.getElementsByTagName('a');
 	var destination;
 	for (var i = 0; i < links.length; i++) {	
 		links[i].onmouseover = function(){
-			destination=this.getAttribute('href');
+			destination = this.getAttribute('href');
 			if (destination.indexOf('index.html') != -1) {
 				moveElement('preview',0,0,5);
 			};
@@ -144,13 +144,13 @@ addLoadEvent(prepareSlideshow);
 
 //获取id,控制section显示隐藏
 function showSection(id){
-	var sections=document.getElementsByTagName('section');
+	var sections = document.getElementsByTagName('section');
 	for (var i = 0; i < sections.length; i++) {
 		if (sections[i].getAttribute('id') != id){
-			sections[i].style.display='none';
+			sections[i].style.display = 'none';
 		}else{
 			// document.getElementById(id).style.display='block';
-			sections[i].style.display='block';
+			sections[i].style.display = 'block';
 		};
 	};
 };
@@ -158,17 +158,17 @@ function showSection(id){
 function prepareInternalnav(){
 	if(!document.getElementsByTagName) return false;
 	if(!document.getElementById) return false;
-	var articles=document.getElementsByTagName('article');
+	var articles = document.getElementsByTagName('article');
 	if (articles.length == 0) return false;
-	var navs=articles[0].getElementsByTagName('nav');
+	var navs = articles[0].getElementsByTagName('nav');
 	if (navs.length == 0) return false;
-	var links=navs[0].getElementsByTagName('a');
+	var links = navs[0].getElementsByTagName('a');
 	for (var i = 0; i < links.length; i++) {
-		var slideId=links[i].href.split('#')[1];
+		var slideId = links[i].href.split('#')[1];
 		if (!document.getElementById(slideId)) continue;
-		document.getElementById(slideId).style.display='none';
-		links[i].destination=slideId;
-		links[i].onmouseover=function(){
+		document.getElementById(slideId).style.display = 'none';
+		links[i].destination = slideId;
+		links[i].onmouseover = function(){
 			showSection(this.destination);
 			return false;
 		};
@@ -181,17 +181,17 @@ addLoadEvent(prepareInternalnav);
 //获取信息添加到相应的标签上
 function showPic(whichpic){
 	if (!document.getElementById('placeholder')) return false;
-	var imagesR=whichpic.getAttribute('href');
-	var placeholder=document.getElementById('placeholder');
+	var imagesR = whichpic.getAttribute('href');
+	var placeholder = document.getElementById('placeholder');
 	placeholder.setAttribute('src',imagesR);
 	if (whichpic.getAttribute('title')) {
-		var text=whichpic.getAttribute('title');
+		var text = whichpic.getAttribute('title');
 	} else{
-		var text='';
+		var text = '';
 	};
-	description=document.getElementById('description');
+	description = document.getElementById('description');
 	if (description.firstChild.nodeType == 3) {
-		description.firstChild.nodeValue=text;
+		description.firstChild.nodeValue = text;
 	};
 	return false;
 };
@@ -201,14 +201,14 @@ function preparePlaceholder(){
 	if (!document.getElementById) return false;
 	if (!document.createTextNode) return false;
 	if (!document.getElementById('imagegallery')) return false;
-	var gallery=document.getElementById('imagegallery');
-	var placeholder=document.createElement('img');
+	var gallery = document.getElementById('imagegallery');
+	var placeholder = document.createElement('img');
 	placeholder.setAttribute('id','placeholder');
 	placeholder.setAttribute('src','images/photos.png');
 	placeholder.setAttribute('alt','images');
-	var description=document.createElement('p');
+	var description = document.createElement('p');
 	description.setAttribute('id','description');
-	var descriptionText=document.createTextNode('choose an images');
+	var descriptionText = document.createTextNode('choose an images');
 	description.appendChild(descriptionText);
 	insertAfter(description,gallery);	
 	insertAfter(placeholder,description);
@@ -218,10 +218,10 @@ function prepareGallery(){
 	if (!document.getElementsByTagName) return false;
 	if (!document.getElementById) return false;
 	if (!document.getElementById('imagegallery')) return false;
-	var gallery=document.getElementById('imagegallery');
-	var links=gallery.getElementsByTagName('a');
+	var gallery = document.getElementById('imagegallery');
+	var links = gallery.getElementsByTagName('a');
 	for (var i = 0; i < links.length; i++) {
-		links[i].onmouseover=function(){
+		links[i].onmouseover = function(){
 			return showPic(this);
 		};
 	};
@@ -235,16 +235,16 @@ addLoadEvent(prepareGallery);
 //隔行换色
 function striptTables(){
 	if (!document.getElementsByTagName) return false;
-	var tables=document.getElementsByTagName('table');
+	var tables = document.getElementsByTagName('table');
 	for (var i = 0; i < tables.length; i++) {
-		var trTag=document.getElementsByTagName('tr');
-		var odd=false;
+		var trTag = document.getElementsByTagName('tr');
+		var odd = false;
 		for (var l = 0; l < trTag.length; l++) {
 			if (odd == true) {
 				addClass(trTag[l],'odd');
-				odd=false;
+				odd = false;
 			} else{
-				odd=true;
+				odd = true;
 			};
 		};
 	};
@@ -268,15 +268,15 @@ function striptTables(){
 //鼠标经过背景色变化
 function highlightRows(){
 	if (!document.getElementsByTagName) return false;
-	var trTag=document.getElementsByTagName('tr');
+	var trTag = document.getElementsByTagName('tr');
 	for (var i = 0; i < trTag.length; i++) {
-		trTag[i].oldClassName=trTag[i].className;
-		trTag[i].onmouseover=function(){
-			this.className='';
+		trTag[i].oldClassName = trTag[i].className;
+		trTag[i].onmouseover = function(){
+			this.className = '';
 			addClass(this,'highlight');
 		};
-		trTag[i].onmouseout=function(){
-			this.className=this.oldClassName;
+		trTag[i].onmouseout = function(){
+			this.className = this.oldClassName;
 		}
 	};
 };
@@ -284,31 +284,31 @@ function highlightRows(){
 //获取关联数组，添加元素节点和文本节点
 function displayAbbreviations(){
 	if (!document.getElementsByTagName || !document.createTextNode || !document.createElement) return false;
-	var abbreviations=document.getElementsByTagName('abbr');
+	var abbreviations = document.getElementsByTagName('abbr');
 	if (abbreviations.length <1) return false;
-	var defs=new Array();
+	var defs = new Array();
 	for (var i = 0; i < abbreviations.length; i++) {
-		var destination=abbreviations[i].getAttribute('title');
+		var destination = abbreviations[i].getAttribute('title');
 		if (destination.length <1) return false;
-		var key=abbreviations[i].firstChild.nodeValue;
-		defs[key]=destination;
+		var key = abbreviations[i].firstChild.nodeValue;
+		defs[key] = destination;
 	};
-	var dllist=document.createElement('dl');
+	var dllist = document.createElement('dl');
 	for (key in defs) {
-		destination=defs[key];
-		var dttitle=document.createElement('dt');
-		var dttitle_text=document.createTextNode(key);
-		var ddtitle=document.createElement('dd');
-		var ddtitle_text=document.createTextNode(destination);
+		destination = defs[key];
+		var dttitle = document.createElement('dt');
+		var dttitle_text = document.createTextNode(key);
+		var ddtitle = document.createElement('dd');
+		var ddtitle_text = document.createTextNode(destination);
 		dttitle.appendChild(dttitle_text);
 		ddtitle.appendChild(ddtitle_text);
 		dllist.appendChild(dttitle);
 		dllist.appendChild(ddtitle);
 	};
-	var articles=document.getElementsByTagName('article')[0];
+	var articles = document.getElementsByTagName('article')[0];
 	if (articles.length < 0) return false;
-	var headers=document.createElement('h3');
-	var headers_text=document.createTextNode('aaaaa');
+	var headers = document.createElement('h3');
+	var headers_text = document.createTextNode('aaaaa');
 	headers.appendChild(headers_text);
 	articles.appendChild(headers);
 	articles.appendChild(dllist);
@@ -322,15 +322,15 @@ addLoadEvent(displayAbbreviations);
 //点击label标签聚焦到相应的input标签
 function focuslabels(){
 	if (!document.getElementsByTagName) return false;
-	var labels=document.getElementsByTagName('label');
+	var labels = document.getElementsByTagName('label');
 	for(var i=0; i< labels.length;i++){
 		if (!labels[i].getAttribute('for')) continue;
-		labels[i].onclick=function(){
-			var id=this.getAttribute('for');
+		labels[i].onclick = function(){
+			var id = this.getAttribute('for');
 			if(!document.getElementById(id)) return false;
-			var element =document.getElementById(id);
+			var element = document.getElementById(id);
 			element.focus();
-			element.style.border='1px solid #444';
+			element.style.border = '1px solid #444';
 		};
 	};
 };
@@ -401,20 +401,20 @@ function validateForm(whichpic){
 // };
 // addLoadEvent(prepareForms);
 
+/*============ajax提交表单============*/
+
+//XMLHttpRequest兼容所有浏览器
 function getHTTPObject(){
-	if ( typeof XMLHttpRequest == 'undefined') {
-		var XMLHttpRequest = function(){
-			try{ return new ActiveXObject('Msxml2.XMLHTTP.6.0');
-			}catch (e){}
-			try{ return new ActiveXObject('Msxml2.XMLHTTP.3.0');
-			}catch (e){}
-			try{ return new ActiveXObject('Msxml2.XMLHTTP');
-			}catch (e){}
-			return false;
-		}
-		return new XMLHttpRequest();
-	};
+	if ( typeof XMLHttpRequest == 'undefined')
+	XMLHttpRequest = function(){
+		try{ return new ActiveXObject('Msxml2.XMLHTTP.6.0');}catch (e){}
+		try{ return new ActiveXObject('Msxml2.XMLHTTP.3.0');}catch (e){}
+		try{ return new ActiveXObject('Msxml2.XMLHTTP');}catch (e){}
+		return false;
+	}
+	return new XMLHttpRequest();
 };
+//移除节点，添加加载图片
 function displayAjaxLoading(element){
 	while (element.hasChildNodes()) {
 		element.removeChild(element.lastChild);
@@ -424,44 +424,45 @@ function displayAjaxLoading(element){
 	content.setAttribute('alt','loading...');
 	element.appendChild(content);
 };
+//ajax表单post请求方法
 function submitFormWithAjax(whichform,thetarget){
-	var request= getHTTPObject();
+	var request = getHTTPObject();
 	if (!request) return false;
 	displayAjaxLoading(thetarget);
-	var dataParts=[];
+	var dataParts = [];
 	var element;
 	for (var i = 0; i < whichform.elements.length; i++) {
 		element = whichform.elements[i];
-		dataParts[i] =element.name + '='+encodeURIComponent(element.value);
+		dataParts[i] = element.name + '='+encodeURIComponent(element.value);
 	};
 	var data = dataParts.join('&');
-	console.log(request);
 	request.open('POST',whichform.getAttribute('action'),true);
 	request.setRequestHeader('Content-type','application/x-www-form-urlencoded');
-	request.onreadystatechange=function(){
+	request.send(data);
+	request.onreadystatechange = function () {
 		if (request.readyState == 4) {
-			if (request.status == 200 || request.status ==0) {
-				var matches =request.responseText.match(/<article>([\s\S]+)<\/article/);
-				if (matches.length>0) {
-					thetarget.innerHTML=matches[1];
-				}else if(matches.length<0){
-					thetarget.innerHTML ='<p>Oops , there was an error. Sorry.</p>'
+			if (request.status == 200 || request.status == 0) {
+				var matches =request.responseText.match(/<article>([\s\S]+)<\/article>/);
+				if (matches.length > 0) {
+					thetarget.innerHTML = matches[1];
 				}else{
-					thetarget.innerHTML='<p>'+request.statusText +'</p>';
+					thetarget.innerHTML = '<p>Oops , there was an error. Sorry.</p>'
 				}
+			}else{
+				thetarget.innerHTML = '<p>'+request.statusText +'</p>';
 			};
 		};
-		request.send(data);
 		return true;
 	};
 };
+//获取forms绑定提交按钮
 function prepareForms(){
 	for (var i = 0; i < document.forms.length; i++) {
-		var thisform=document.forms[i];
+		var thisform = document.forms[i];
 		// resetFields(thisform);
-		thisform.onsubmit=function(){
+		thisform.onsubmit = function(){
 			if (!validateForm(this)) return false;
-			var article= document.getElementsByTagName('article')[0];
+			var article = document.getElementsByTagName('article')[0];
 			if (submitFormWithAjax(this,article)) return false;
 			return true;
 		};
