95d96bbd1ad2c0eee8078ac47ae692f8fb34dcf1
John Doe <fangfang.ouyang@upai.com>
2013-11-19 16:12:05 +0800

    change js

---
+++ scripts/script.js
@@ -0,0 +1,4 @@
+(function(){
+	document.writeln('<script type="text/javascript" src="scripts/global.js"</script>');
+	document.writeln('<script type="text/javascript" src="scripts/modernizr-2.6.3.js"></script>')
+}).call(this);
